debc9a446d76618239121eb328522125c1b36f2b
Eileen Yoon <eyn@gmx.com>
2023-03-20 17:01:48 +0900

    anecc: separate compile functions
    Signed-off-by: Eileen Yoon <eyn@gmx.com>

--- anecc/anecc/__init__.py
+++ anecc/anecc/__init__.py
@@ -30,13 +30,92 @@ LIBANE_HDR = "/usr/include/libane"
 LIBANE_OBJ = "/usr/lib/libane.o"  # python doesn't like archives
 
 
+def _anecc_compile_c(name, outdir, tmpdir):
+
+	logger.info('compiling for C/C++...')
+
+	kern_obj = f'{name}.anec.o'
+	shutil.copyfile(kern_obj, os.path.join(outdir, kern_obj))
+	logger.info(f'created kernel object: {os.path.join(outdir, kern_obj)}')
+
+	# all but the init call is model-specific (duh)
+	# so compile that & generate a header for it
+	init_obj = os.path.join(tmpdir, f'anec_{name}.o')
+	init_src = os.path.join(tmpdir, f'anec_{name}.c')  # tmp
+	with open(init_src, "w") as f:
+		f.write(f'#include "ane.h"\n')
+		f.write(f'#include "anec_{name}.h"\n')
+
+	cmd = f'{CC} -I/{LIBDRM_HDR}' \
+		f' -I/{DRIVER_HDR} -I/{LIBANE_HDR}' \
+		f' -c -o {init_obj} {init_src}'
+	logger.info(cmd)
+	subprocess.run(shlex.split(cmd))
+
+	obj_path = os.path.join(outdir, f'anec_{name}.o')
+	shutil.copyfile(init_obj, obj_path)
+	logger.info(f'created anec object: {obj_path}')
+
+	hdr = "#ifndef __ANEC_%s_H__\n" \
+		"#define __ANEC_%s_H__\n" \
+		"\n" \
+		"#if defined(__cplusplus)\n" \
+		"extern \"C\" {\n" \
+		"#endif\n" \
+		"\n" \
+		"#include \"ane.h\"\n" \
+		"struct ane_nn *ane_init_%s(void);\n" \
+		"\n" \
+		"#if defined(__cplusplus)\n" \
+		"}\n" \
+		"#endif\n" \
+		"\n" \
+		"#endif /* __ANEC_%s_H__ */\n" % (name.upper(), name.upper(), name, name.upper())
+
+	hdr_path = os.path.join(outdir, f'anec_{name}.h')
+	with open(hdr_path, "w") as f:
+		f.write(hdr)
+	logger.info(f'created anec header: {hdr_path}')
+
+	return
+
+
+def _anecc_compile_python(name, outdir, tmpdir):
+
+	logger.info('compiling for Python...')
+
+	# again, compile the init call
+	dylib_name = f'{name}.anec.so'
+	dylib_obj = os.path.join(tmpdir, dylib_name)
+	dylib_src = os.path.join(tmpdir, f'pyane_{name}.c')  # tmp
+	with open(dylib_src, "w") as f:
+		f.write(f'#include "pyane.h"\n')
+		f.write(f'#include "anec_{name}.h"\n')
+		f.write('void *pyane_init(void) { return ane_init_%s(); }\n' % name)
+
+	# compile completed dylib
+	cmd = f'{CC} -shared -pthread -fPIC -fno-strict-aliasing -I.' \
+		f' -I/{PYTHON_HDR} -I/{LIBDRM_HDR}' \
+		f' -I/{DRIVER_HDR} -I/{LIBANE_HDR}' \
+		f' {LIBANE_OBJ} {name}.anec.o' \
+		f' {dylib_src} -o {dylib_obj}'
+	logger.info(cmd)
+	subprocess.run(shlex.split(cmd))
+
+	# save the thing
+	dylib_path = os.path.join(outdir, dylib_name)
+	shutil.copyfile(dylib_obj, dylib_path)
+	logger.info(f'created dylib: {dylib_path}')
+
+	return
+
+
 def anecc_compile(path, name="model", outdir="", c=False, python=False):
 
 	if (platform.system() != "Linux"):
 		logger.warn("compiling is only supported on Linux.")
 
 	res = anect_convert(path, name=name)
-	name = res.name  # override with sanitized name
 
 	if (platform.system() != "Linux"):
 		logger.warn("Model can convert successfully. Re-run anecc in Linux.")
@@ -45,97 +124,22 @@ def anecc_compile(path, name="model", outdir="", c=False, python=False):
 		logger.warn("Nothing to compile. See anecc --help for available options.")
 		return
 
-	if (not outdir):
-		outdir = os.getcwd()
-	else:
-		outdir = os.path.abspath(outdir)
+	name = res.name  # override with sanitized name
+	outdir = os.path.abspath(outdir)
 
 	with tempfile.TemporaryDirectory() as tmpdir:
 		anect_write(res, prefix=tmpdir)
-
-		anec_obj = f'{name}.anec.o'
-		anec_hdr = f'anec_{name}.h'
-
 		os.chdir(tmpdir)
 
 		# See "Embedding resources in executable using GCC"
 		# https://stackoverflow.com/a/4158997
 		# Since ML weights are fucking massive,
 		# we abuse ld to load them @ compile time.
-		cmd = f'ld -r -b binary -o {anec_obj} {name}.anec'
+		cmd = f'ld -r -b binary -o {name}.anec.o {name}.anec'
 		logger.debug(cmd)
 		subprocess.run(shlex.split(cmd))
 
 		if (c):
-			logger.info('compiling for C/C++...')
-
-			# save kernel object
-			shutil.copyfile(anec_obj, os.path.join(outdir, anec_obj))
-			logger.info(f'created kernel object: {os.path.join(outdir, anec_obj)}')
-
-			# all but the init call is model-specific (duh)
-			# so compile that & generate a header for it
-			init_obj = os.path.join(tmpdir, f'anec_{name}.o')
-			init_src = os.path.join(tmpdir, f'anec_{name}.c')  # tmp
-			with open(init_src, "w") as f:
-				f.write(f'#include "ane.h"\n')
-				f.write(f'#include "{anec_hdr}"\n')
-
-			cmd = f'{CC} -I/{LIBDRM_HDR}' \
-				f' -I/{DRIVER_HDR} -I/{LIBANE_HDR}' \
-				f' -c -o {init_obj} {init_src}'
-			logger.info(cmd)
-			subprocess.run(shlex.split(cmd))
-
-			# save init call object
-			obj_path = os.path.join(outdir, f'anec_{name}.o')
-			shutil.copyfile(init_obj, obj_path)
-			logger.info(f'created anec object: {obj_path}')
-
-			# write the header to access the init call
-			hdr = "#ifndef __ANEC_%s_H__\n" \
-				"#define __ANEC_%s_H__\n" \
-				"\n" \
-				"#if defined(__cplusplus)\n" \
-				"extern \"C\" {\n" \
-				"#endif\n" \
-				"\n" \
-				"#include \"ane.h\"\n" \
-				"struct ane_nn *ane_init_%s(void);\n" \
-				"\n" \
-				"#if defined(__cplusplus)\n" \
-				"}\n" \
-				"#endif\n" \
-				"\n" \
-				"#endif /* __ANEC_%s_H__ */\n" % (name.upper(), name.upper(), name, name.upper())
-			hdr_path = os.path.join(outdir, f'anec_{name}.h')
-			with open(hdr_path, "w") as f:
-				f.write(hdr)
-			logger.info(f'created anec header: {hdr_path}')
-
-
+			_anecc_compile_c(name, outdir, tmpdir)
 		if (python):
-			logger.info('compiling for Python...')
-
-			# again, compile the init call
-			pyane_obj_name = f'{name}.anec.so'
-			pyane_obj = os.path.join(tmpdir, pyane_obj_name)
-			pyane_src = os.path.join(tmpdir, f'pyane_{name}.c')  # tmp
-			with open(pyane_src, "w") as f:
-				f.write(f'#include "pyane.h"\n')  # contains pyane funcs
-				f.write(f'#include "{anec_hdr}"\n')
-				f.write('void *pyane_init(void) { return ane_init_%s(); }\n' % name)
-
-			# compile completed pyane to dylib
-			cmd = f'{CC} -shared -pthread -fPIC -fno-strict-aliasing -I.' \
-				f' -I/{PYTHON_HDR} -I/{LIBDRM_HDR}' \
-				f' -I/{DRIVER_HDR} -I/{LIBANE_HDR}' \
-				f' {LIBANE_OBJ} {anec_obj}' \
-				f' {pyane_src} -o {pyane_obj}'
-			logger.info(cmd)
-			subprocess.run(shlex.split(cmd))
-
-			# save the thing
-			obj_path = os.path.join(outdir, pyane_obj_name)
-			shutil.copyfile(pyane_obj, obj_path)
-			logger.info(f'created dylib oject: {obj_path}')
+			_anecc_compile_python(name, outdir, tmpdir)
