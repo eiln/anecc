59a1931a8b25c219dc4a02bb1f75a3def25a389d
Eileen Yoon <eyn@gmx.com>
2023-03-24 07:46:42 +0900

    anecc: add CFLAGS
    Signed-off-by: Eileen Yoon <eyn@gmx.com>

--- anecc/anecc/__init__.py
+++ anecc/anecc/__init__.py
@@ -23,6 +23,7 @@ logging.addLevelName(logging.WARNING, 'warn')
 logger = logging.getLogger(__name__)
 
 CC = "gcc"
+CFLAGS = "-I. -std=gnu99"
 PYTHON_HDR = sysconfig.get_paths()['include']  # "/usr/include/python3.10"
 DRIVER_HDR = "/home/eileen/ane/ane/src/include"  # should be resolved w/ accel?
 LIBANE_HDR = "/usr/include/libane"
@@ -45,7 +46,7 @@ def _anecc_compile_c(name, outdir, tmpdir):
 		f.write(f'#include "ane.h"\n')
 		f.write(f'#include "anec_{name}.h"\n')
 
-	cmd = f'{CC} -I/{DRIVER_HDR} -I/{LIBANE_HDR}' \
+	cmd = f'{CC} {CFLAGS} -I/{DRIVER_HDR} -I/{LIBANE_HDR}' \
 		f' -c -o {init_obj} {init_src}'
 	logger.info(cmd)
 	subprocess.run(shlex.split(cmd))
@@ -92,8 +93,8 @@ def _anecc_compile_python(name, outdir, tmpdir):
 		f.write('void *pyane_init(void) { return ane_init_%s(); }\n' % name)
 
 	# compile completed dylib
-	cmd = f'{CC} -shared -pthread -fPIC -fno-strict-aliasing' \
-		f' -I. -I/{PYTHON_HDR} -I/{DRIVER_HDR} -I/{LIBANE_HDR}' \
+	cmd = f'{CC} {CFLAGS} -shared -pthread -fPIC -fno-strict-aliasing' \
+		f' -I/{PYTHON_HDR} -I/{DRIVER_HDR} -I/{LIBANE_HDR}' \
 		f' {LIBANE_OBJ} {name}.anec.o' \
 		f' {dylib_src} -o {dylib_obj}'
 	logger.info(cmd)
